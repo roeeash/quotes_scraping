b3b1531645efbd21f58a6924d0c31bb2dc3f3fc2
roeeash <92747574+roeeash@users.noreply.github.com>
2025-12-09 17:37:43 +0200

    add simple extraction

---
+++ extractor.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from types.Quote import Quote
+from utils import parse_quote_items
+
+
+def extract_quote_data_from_page(page:int) -> List[Quote]:
+    driver = webdriver.Chrome()
+    driver.get(f"https://quotes.toscrape.com/page/{page}")
+    quotes_data = driver.find_elements(By.XPATH, "(//div[@class='quote'])")
+    quotes = []
+
+    for quote_data in quotes_data:
+        quote_data_text = quote_data.text
+        quote_data_parts = quote_data_text.split("\n")
+        quote = parse_quote_items(quote_data_parts=quote_data_parts)
+        quotes.append(quote)
+
+    return quotes
+
+
+
+
+
+def extract_quote_data_from_page_range(first_page: int, last_page: int) -> List[Quote]:
+    all_quotes = []
+
+    for page in range(first_page, last_page + 1):
+        quotes_from_page = extract_quote_data_from_page(page=page)
+        for quotes in quotes_from_page:
+            all_quotes.append(quotes)
+
+    return all_quotes
+
+
+
+
+extract_quote_data_from_page_range(first_page=1, last_page=1)
---
+++ utils.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from types.Quote import Quote
+
+
+def parse_quote_items(quote_data_parts: List[str]) -> Quote:
+    raw_text = quote_data_parts[0]
+    text = raw_text.strip().strip('“”')
+
+    author = None
+    if len(quote_data_parts) > 1:
+        author_line = quote_data_parts[1].strip()
+        if author_line.lower().startswith("by "):
+            author_line = author_line[3:]
+        if "(" in author_line:
+            author_line = author_line.split("(")[0].strip()
+        author = author_line
+
+    tags = []
+    if len(quote_data_parts) > 2:
+        tags_line = quote_data_parts[2]
+        if "Tags:" in tags_line:
+            tags_line = tags_line.strip()[5:]
+            tags_line = tags_line.strip().split(" ")
+            tags = tags_line
+
+    return Quote(text=text, author=author, tags=tags)
